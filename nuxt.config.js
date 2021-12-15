fcbc80f5d30da386220ebac8246039f5191af806
Silvia <silviamonti@hotmail.com>
2021-12-15 17:48:37 +0100

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio-nuxt',
+    title: 'Silvia Monti — Design & Code',
     htmlAttrs: {
       lang: 'en'
     },
